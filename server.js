3e1e13e8a4b56e1a3dc4c15a7fbb06741e315eaa
Panchanok Kaewchinda <kpanchanok@myseneca.ca>
2023-01-13 14:25:02 -0500

    edit name and add declare

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Panchanok Kaewchinda Student ID: 145443214 Date: 13-01-2023
+*
+*  Online (Cyclic) URL: https://brown-badger-toga.cyclic.app
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send("Panchanok Kaewvhinda - 145443214");
+    res.send("Panchanok Kaewchinda - 145443214");
 });
 
 // setup http server to listen on HTTP_PORT
